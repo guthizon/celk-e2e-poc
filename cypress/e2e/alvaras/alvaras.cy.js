3fe275a88ba0dd25afd6f635291a9275fc4290b9
Gustavo <gthizon@gmail.com>
2025-02-27 17:01:35 -0300

    first commit

---
+++ cypress/e2e/alvaras/alvaras.cy.js
@@ -0,0 +1,17 @@
+describe('Manter Dados de Alvarás', { testIsolation: false }, () => {
+    it('Emitir novo alvará', () => {
+        cy.clearAllCookies();
+        cy.clearAllSessionStorage();
+        cy.clearAllLocalStorage();
+        cy.visit('/');
+        cy.login();
+        cy.selecionarEntidade("ACADEMIA DA SAUDE: MAFRA EM FORMA");
+        cy.acessarMenu("Vigilância");
+        cy.acessarSubMenu("Vigilância Sanitária");
+        cy.acessarRotina("Requerimento/Protocolo");
+        cy.solicitarDocumento("Alvará");
+        cy.selecionarTipoAlvara("Alvará Inicial");
+        cy.informarDadosParaEmissaoAlvara();
+        cy.validarMensagemExibida('Já existe um alvará inicial para o estabelecimento');
+    })
+})
