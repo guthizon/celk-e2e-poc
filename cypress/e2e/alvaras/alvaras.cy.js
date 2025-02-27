0d34031d2923506ca2a1249553ab48f50b8981c2
Gustavo <gthizon@gmail.com>
2025-02-27 19:40:19 -0300

    add new fail scenario

--- cypress/e2e/alvaras/alvaras.cy.js
+++ cypress/e2e/alvaras/alvaras.cy.js
@@ -14,4 +14,19 @@ describe('Manter Dados de Alvarás', { testIsolation: false }, () => {
         cy.informarDadosParaEmissaoAlvara();
         cy.validarMensagemExibida('Já existe um alvará inicial para o estabelecimento');
     })
+    it('Emitir novo alvará falhar teste', () => {
+        cy.clearAllCookies();
+        cy.clearAllSessionStorage();
+        cy.clearAllLocalStorage();
+        cy.visit('/');
+        cy.login();
+        cy.selecionarEntidade("ACADEMIA DA SAUDE: MAFRA EM FORMA");
+        cy.acessarMenu("tesssste");
+        cy.acessarSubMenu("Vigilância Sanitária");
+        cy.acessarRotina("Requerimento/Protocolo");
+        cy.solicitarDocumento("Alvará");
+        cy.selecionarTipoAlvara("Alvará Inicial");
+        cy.informarDadosParaEmissaoAlvara();
+        cy.validarMensagemExibida('Já existe um alvará inicial para o estabelecimento');
+    })
 })
