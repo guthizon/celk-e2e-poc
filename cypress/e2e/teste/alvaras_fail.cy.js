ab240da2b084359dcd579c61e01cba225449a383
Gustavo <gthizon@gmail.com>
2025-02-28 11:00:47 -0300

    alterado pastas

--- cypress/e2e/teste/alvaras_fail.cy.js
+++ cypress/e2e/teste/alvaras_fail.cy.js
@@ -1,17 +1,12 @@
 describe('Manter Dados de Alvarás fail', { testIsolation: false }, () => {
-    it('Emitir novo alvará ddd', () => {
+    it('Emitir novo alvará Fail', () => {
         cy.clearAllCookies();
         cy.clearAllSessionStorage();
         cy.clearAllLocalStorage();
         cy.visit('/');
         cy.login();
         cy.selecionarEntidade("ACADEMIA DA SAUDE: MAFRA EM FORMA");
-        cy.acessarMenu("Vigilância");
-        cy.acessarSubMenu("Vigilância Sanitária");
-        cy.acessarRotina("Requerimento/Protocolo");
-        cy.solicitarDocumento("Alvará");
-        cy.selecionarTipoAlvara("Alvará Inicial");
-        cy.informarDadosParaEmissaoAlvara();
-        cy.validarMensagemExibida('Já existe um alvará inicial para o estabelecimento');
+        cy.acessarMenu("Tessssst");
+       
     })
 })
